6982fbdb97820d0b16612a481fa5e597f99150cf
unknown <gabrielyan3g@gmail.com>
2020-11-02 14:16:54 +0400

    BrowserRouter, Routh component, path, NavLink to

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,19 +1,40 @@
 import React from 'react';
-import ListItem from './ListItem/ListItem';
+// import ListItem from './ListItem/ListItem';
 import classes from './Navbar.module.css';
 import menu from './menu.svg'
+import { NavLink } from 'react-router-dom';
 
-function Navbar() {
+function Navbar(props) {
     return (
 
 
         <div className={classes.navbar__wrapper}>
             <ul>
-                <ListItem name='Profile' hr='/Profile' />
-                <ListItem name='Massages' hr='/Massages' />
+                <li>
+                    <div className={classes.list__dot}></div>
+                    <NavLink to='/profile'>Profile</NavLink>
+                </li>
+                <li>
+                    <div className={classes.list__dot}></div>
+                    <NavLink to='/massages'>Massages</NavLink>
+                </li>
+                <li>
+                    <div className={classes.list__dot}></div>
+                    <NavLink to='/news'>News</NavLink>
+                </li>
+                <li>
+                    <div className={classes.list__dot}></div>
+                    <NavLink to='/settings'>Settings</NavLink>
+                </li>
+                <li>
+                    <div className={classes.list__dot}></div>
+                    <NavLink to='/FAQ'>FAQ</NavLink>
+                </li>
+                {/* <ListItem name='Profile' na='/Profile' />
+                <ListItem name='Massages' na='/Massages' />
                 <ListItem name='News' />
                 <ListItem name='Settings' />
-                <ListItem name='FAQ' />
+                <ListItem name='FAQ' /> */}
             </ul>
         </div>
 
