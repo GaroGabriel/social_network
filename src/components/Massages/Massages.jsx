695574650e174529506cff0a8443b6d56d3afa6b
unknown <gabrielyan3g@gmail.com>
2020-11-04 18:16:28 +0400

    MAP, array

--- src/components/Massages/Massages.jsx
+++ src/components/Massages/Massages.jsx
@@ -2,46 +2,64 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import classes from './Massages.module.css'
 
-function Massages(props) {
+
+//////////////////////////////////////////////////////////////// DIALOGE
+const Dialoge = (props) => {
+
+    const toLink = '/massages/' + props.num;
+
+    return (
+        <div className={classes.dialog}>
+            <NavLink to={toLink} className={classes.dialog__person + ' ' + props.active}>
+                {props.name}</NavLink>
+        </div>
+    )
+}
+
+//////////////////////////////////////////////////////////////// MASSAGE
+const Massage = (props) => {
+
+    return (
+        <div className={classes.massage}>
+            {props.massage}
+        </div>
+    )
+}
+
+
+//////////////////////////////////////////////////////////////// MASSAGEsSs
+const Massages = (props) => {
+
+    const dialogeArr = [
+        { name: 'ArmanCHO', id: 1, active: classes.active },
+        { name: 'Հայկուհի', id: 2, active: '' },
+        { name: 'Ինչ-որ հավայի Group', id: 3, active: '' },
+        { name: '2Pac', id: 4, active: '' },
+        { name: 'ԱԱԾ', id: 5, active: '' }
+    ]
+    const massageArr = [
+        { id: 1, massage: 'first massage' },
+        { id: 2, massage: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit quisquam numquam distinctio asperiores  porro harum facere aliquid veritatis, repellendus fugiat voluptatum dolor ut illo cum cupiditate est. Voluptatibus, corrupti placeat.' },
+
+        { id: 3, massage: 'ոռաթիո դելենիթ ծոնսթիթուամ մեա թե, մեա եու ուլլամծոռպեռ սուսծիպիանթուռ. նո դեսեռունթ վուլպութաթե վոլուպթաթում եում. եամ եխ ասսում թիմեամ աեթեռնո, իուս եթ նեմոռե բլանդիթ պռոբաթուս. սոլում նուլլամ իմպեռդիեթ վիմ եի, եռաթ ծոպիոսաե պոնդեռում վիմ եի, ալիի սապիենթեմ քուո ին.' },
+    ]
+
+    let dialogMap = dialogeArr.map((i) => <Dialoge num={i.id} name={i.name} active={i.active} />)
+    let massageMap = massageArr.map((i) => <Massage massage={i.massage} id={i.id} />)
+
     return (
         <div className={classes.massages__wrapper}>
             <div className={classes.dialogs}>
                 <h3>Messenger</h3>
-                <div className={classes.dialog}>
-                    <NavLink to='/massages/1' className={classes.dialog__person + ' ' + classes.active}>
-                        ArmanCHO</NavLink>
-                </div>
-                <div className={classes.dialog}>
-                    <NavLink to='/massages/2' className={classes.dialog__person}>
-                        Հայկուհի</NavLink>
-                </div>
-                <div className={classes.dialog}>
-                    <NavLink to='/massages/3' className={classes.dialog__person}>
-                        Ինչ-որ հավայի Group</NavLink>
-                </div>
-                <div className={classes.dialog}>
-                    <NavLink to='/massages/4' className={classes.dialog__person}>
-                        2Pac</NavLink>
-                </div>
-                <div className={classes.dialog}>
-                    <NavLink to='/massages/5' className={classes.dialog__person}>
-                        ԱԱԾ</NavLink>
-                </div>
+
+                {dialogMap}
+
             </div>
 
 
             <div className={classes.massages}>
                 <div className={classes.person__massages}>
-                    <div className={classes.massage}>
-                        massages 1
-                    </div>
-                    <div className={classes.massage}>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit quisquam numquam distinctio asperiores porro harum facere aliquid veritatis, repellendus fugiat voluptatum dolor ut illo cum cupiditate est. Voluptatibus, corrupti placeat.
-                    </div>
-                    <div className={classes.massage}>
-                        ոռաթիո դելենիթ ծոնսթիթուամ մեա թե, մեա եու ուլլամծոռպեռ սուսծիպիանթուռ. նո դեսեռունթ վուլպութաթե վոլուպթաթում եում. եամ եխ ասսում թիմեամ աեթեռնո, իուս եթ նեմոռե բլանդիթ պռոբաթուս. սոլում նուլլամ իմպեռդիեթ վիմ եի, եռաթ ծոպիոսաե պոնդեռում վիմ եի, ալիի սապիենթեմ քուո ին.
-                    </div>
-
+                    {massageMap}
                 </div>
             </div>
         </div>
--- src/components/Profile/Wall/Wall_posts/Wall_posts.jsx
+++ src/components/Profile/Wall/Wall_posts/Wall_posts.jsx
@@ -2,37 +2,40 @@ import React from 'react';
 import classes from './Wall_posts.module.css';
 import Post from './Post/Post'
 function Wall_posts() {
+
+    const post = [
+        {
+            id: 1, avatar: 'https://zbyhoo.files.wordpress.com/2011/07/me_pixel_bright_bg.png?w=584',
+            name: 'Artush',
+            content: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aut quae suscipit deleniti molestias illo! Perferendis distinctio pariatur quas autem nihil.',
+            like: 12
+        },
+        {
+            id: 2,
+            avatar: 'https://www.spletnik.ru/thumb/310x310/img/persons/Eminem-post.jpg',
+            name: 'Eminem Dzya',
+            content: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aut quae suscipit deleniti molestias illo! Perferendis distinctio pariatur quas autem nihil.Aut quae suscipit deleniti molestias illo! Aut quae suscipit deleniti molestias illo! ',
+            like: 10
+        },
+        {
+            id: 3,
+            avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQYHlYk5OZwPplU5Gra-NXSZQef1R3OAb7Z9A&usqp=CAU',
+            name: 'Ջիվան Գասպարյան',
+            content: 'լոռեմ իպսում դոլոռ սիթ ամեթ, ին ֆուգիթ ածծուսամուս ծոմպռեհենսամ վել, քուո մելիոռե մոդեռաթիուս եա. եա իդքուե ալթեռա սիգնիֆեռումքուե մել. սալե ոմնիս պռո եու, եու ալիքուիդ պեթենթիում իուս. քուաս լաբոռես սեդ ծու, թե ուբիքուե դեբիթիս ֆուիսսեթ ուսու, եում ադ ոռնաթուս եֆֆիծիենդի. եում ան դիսսենթիունթ դիսպութաթիոնի. պեռ հենդռեռիթ լիբեռավիսսե եի, թե գռաեծո մենանդռի հաս.',
+            like: 122
+        },
+    ]
+    let postM = post.map((p) =>
+        < Post avatar={p.avatar} name={p.name} content={p.content} like={p.like} />
+    )
+
     return (
         <div className={classes.wall_posts__wrapper}>
-            <Post
-                avatar='https://zbyhoo.files.wordpress.com/2011/07/me_pixel_bright_bg.png?w=584'
-
-                name='Shredr'
-
-                content='Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aut quae suscipit deleniti molestias illo! Perferendis distinctio pariatur quas autem nihil.'
-
-                like='12' />
-
-
-            <Post
-                avatar='https://www.spletnik.ru/thumb/310x310/img/persons/Eminem-post.jpg'
-
-                name='Eminem Dzya'
-
-                content='Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aut quae suscipit deleniti molestias illo! Perferendis distinctio pariatur quas autem nihil.Aut quae suscipit deleniti molestias illo! Aut quae suscipit deleniti molestias illo! '
-                like='10' />
-            <Post
-                avatar='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQYHlYk5OZwPplU5Gra-NXSZQef1R3OAb7Z9A&usqp=CAU'
-
-                name='Ջիվան Գասպարյան'
-
-                content='լոռեմ իպսում դոլոռ սիթ ամեթ, ին ֆուգիթ ածծուսամուս ծոմպռեհենսամ վել, քուո մելիոռե մոդեռաթիուս եա. եա իդքուե ալթեռա սիգնիֆեռումքուե մել. սալե ոմնիս պռո եու, եու ալիքուիդ պեթենթիում իուս. քուաս լաբոռես սեդ ծու, թե ուբիքուե դեբիթիս ֆուիսսեթ ուսու, եում ադ ոռնաթուս եֆֆիծիենդի. եում ան դիսսենթիունթ դիսպութաթիոնի. պեռ հենդռեռիթ լիբեռավիսսե եի, թե գռաեծո մենանդռի հաս.'
-                like='1256' />
-
-
-
+            {postM}
         </div>
+
     );
+    { console.log(postM) };
 }
 
 export default Wall_posts;
